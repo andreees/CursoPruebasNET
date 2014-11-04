fed125e48db3648fa302944bfc160a58b37bd5e4
andreees <u201215083@upc.edu.pe>
2014-11-03 23:05:44 -0500

    CRUD con MySql Implementado

---
+++ UPC.CursoPruebas.DL.DALC/CursoDALC.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using UPC.CursoPruebas.BL.BE;
+
+//using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+using MySql.Data.MySqlClient;
+
+namespace UPC.CursoPruebas.DL.DALC
+{
+    public class CursoDALC
+    {
+        public List<CursoBE> ListarCursos()
+        {
+            String query;
+            String sConn;
+            List<CursoBE> L;
+            try
+            {
+                sConn = ConfigurationManager.ConnectionStrings["CSCursoPruebas"].ToString();
+                MySqlConnection con = new MySqlConnection(sConn);
+                query = "SELECT * FROM curso";
+                MySqlDataAdapter da = new MySqlDataAdapter(query, con);
+                DataSet ds = new DataSet();
+                da.Fill(ds,"curso");
+                
+                L = new List<CursoBE>();
+                for (int i = 0;i<ds.Tables[0].Rows.Count ;i++)
+                {
+                    CursoBE curso = new CursoBE();
+                    curso.IdCurso = Int32.Parse(ds.Tables[0].Rows[i]["idcurso"].ToString());
+                    curso.Nombre = ds.Tables[0].Rows[i]["nombre"].ToString();
+                    curso.Codigo = ds.Tables[0].Rows[i]["codigo"].ToString();
+                    curso.Creditos = Int32.Parse(ds.Tables[0].Rows[i]["creditos"].ToString());
+                    curso.Requisitos = ds.Tables[0].Rows[i]["requisitos"].ToString();
+                    curso.Ciclo = Int32.Parse(ds.Tables[0].Rows[i]["ciclo"].ToString());
+                    L.Add(curso);
+                }
+                ds.Dispose();
+                return L;
+            }
+            catch (Exception ex)
+            {
+                
+                throw;
+            }
+        }
+        
+        public CursoBE ObtenerCursoPorid(int id)
+        {
+            String query;
+            String sConn;
+
+            try
+            {
+                sConn = ConfigurationManager.ConnectionStrings["CSCursoPruebas"].ToString();
+                MySqlConnection con = new MySqlConnection(sConn);
+                query = "SELECT * FROM curso WHERE idcurso=" + id.ToString();
+                MySqlDataAdapter da = new MySqlDataAdapter(query, con);
+                DataSet ds = new DataSet();
+                da.Fill(ds, "curso");
+
+                CursoBE curso = new CursoBE();
+                curso.IdCurso = Int32.Parse(ds.Tables[0].Rows[0]["idcurso"].ToString());
+                curso.Nombre = ds.Tables[0].Rows[0]["nombre"].ToString();
+                curso.Codigo = ds.Tables[0].Rows[0]["codigo"].ToString();
+                curso.Creditos = Int32.Parse(ds.Tables[0].Rows[0]["creditos"].ToString());
+                curso.Requisitos = ds.Tables[0].Rows[0]["requisitos"].ToString();
+                curso.Ciclo = Int32.Parse(ds.Tables[0].Rows[0]["ciclo"].ToString());
+
+                if (curso.Nombre == null)
+                {
+                    curso = null;
+                }
+                ds.Dispose();
+                return curso;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+        
+        public bool InsertarCurso(CursoBE objCursoBE)
+        {
+            String query;
+            String sConn;
+
+            try
+            {
+                sConn = ConfigurationManager.ConnectionStrings["CSCursoPruebas"].ToString();
+                MySqlConnection con = new MySqlConnection(sConn);
+                query = "INSERT INTO curso(idcurso, nombre, codigo, creditos, requisitos, ciclo) VALUES('"+null+"','"+objCursoBE.Nombre+"','"+objCursoBE.Codigo+"',"+objCursoBE.Creditos.ToString()+",'"+objCursoBE.Requisitos+"',"+objCursoBE.Ciclo.ToString()+")";
+                MySqlDataAdapter da = new MySqlDataAdapter(query, con);
+                DataSet ds = new DataSet();
+                da.Fill(ds, "curso");
+                ds.Dispose();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+                throw;
+            }
+        }
+        
+        public bool ActualizarCurso(CursoBE objCursoBE)
+        {
+            String query;
+            String sConn;
+
+            try
+            {
+                sConn = ConfigurationManager.ConnectionStrings["CSCursoPruebas"].ToString();
+                MySqlConnection con = new MySqlConnection(sConn);
+                query = "UPDATE curso SET nombre='"+objCursoBE.Nombre+"',codigo='"+objCursoBE.Codigo+"',creditos="+objCursoBE.Creditos.ToString()+",requisitos='"+objCursoBE.Requisitos+"',ciclo="+objCursoBE.Ciclo.ToString()+" WHERE idcurso="+objCursoBE.IdCurso.ToString();
+                MySqlDataAdapter da = new MySqlDataAdapter(query, con);
+                DataSet ds = new DataSet();
+                da.Fill(ds, "curso");
+                ds.Dispose();
+                return true;
+
+                //if (ObtenerCursoPorid(objCursoBE.IdCurso) == null)
+                  //  return;
+
+            
+            }
+            catch(Exception ex)
+            {
+                return false;
+                throw;
+            }
+        }
+
+        public bool EliminarCurso(int id)
+        {
+            String query;
+            String sConn;
+
+            try
+            {
+                sConn = ConfigurationManager.ConnectionStrings["CSCursoPruebas"].ToString();
+                MySqlConnection con = new MySqlConnection(sConn);
+                query = "DELETE FROM curso WHERE idcurso="+id.ToString();
+                MySqlDataAdapter da = new MySqlDataAdapter(query, con);
+                DataSet ds = new DataSet();
+                da.Fill(ds, "curso");
+                ds.Dispose();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+                throw;
+            }
+        }
+    }
+
+
+}
---
+++ UPC.CursoPruebas.PL.UI.AppConsole/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using UPC.CursoPruebas.BL.BE;
+using UPC.CursoPruebas.DL.DALC;
+
+namespace UPC.CursoPruebas.PL.UI.AppConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //if (InsertarCurso("aeiou", "s545", 5, "MA6", 5))
+            //    Console.WriteLine("EXITO");
+            //else
+            //    Console.WriteLine("FALLO");
+
+            //if (ActualizarCurso(5,"AAAAAAA", "s545", 5, "MA6", 5))
+            //    Console.WriteLine("EXITO");
+            //else
+            //    Console.WriteLine("FALLO");
+
+            //if(EliminarCurso(13))
+            //    Console.WriteLine("EXITO");
+            //else
+            //    Console.WriteLine("FALLO");
+            InsertarCurso("xxx", "xxx", 69, "xxx", 69);
+
+        }
+        static void ListarTodo()
+        {
+            List<CursoBE> Lista;
+            CursoDALC objUDALC;
+            try
+            {
+                objUDALC = new CursoDALC();
+                Lista = objUDALC.ListarCursos();
+                Console.WriteLine("Datos Curso: ");
+                Console.WriteLine("-----------------");
+                for (int i = 0; i < Lista.Count; i++)
+                {
+                    Console.WriteLine("id de Curso: " + Lista[i].IdCurso.ToString());
+                    Console.WriteLine("Nombre: " + Lista[i].Nombre);
+                    Console.WriteLine("Codigo: " + Lista[i].Codigo);
+                    Console.WriteLine("Creditos: " + Lista[i].Creditos.ToString());
+                    Console.WriteLine("Requisitos: " + Lista[i].Requisitos);
+                    Console.WriteLine("Ciclo: " + Lista[i].Ciclo.ToString());
+                    Console.WriteLine();
+                }
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(":o hubo un problema!");
+
+                return;
+            }
+        }
+
+        static void ObtenerCursoPorid(int id)
+        {
+            CursoDALC cur=new CursoDALC();
+            CursoBE curso=cur.ObtenerCursoPorid(id);
+            
+            Console.WriteLine("id de Curso: " + curso.IdCurso.ToString());
+            Console.WriteLine("Nombre: " + curso.Nombre);
+            Console.WriteLine("Codigo: " + curso.Codigo);
+            Console.WriteLine("Creditos: " + curso.Creditos.ToString());
+            Console.WriteLine("Requisitos: " + curso.Requisitos);
+            Console.WriteLine("Ciclo: " + curso.Ciclo.ToString());
+            Console.WriteLine();
+        }
+
+        static bool InsertarCurso(String nombre, String codigo, int creditos, String requisitos, int ciclo)
+        {
+            CursoDALC cur = new CursoDALC();
+            CursoBE curso=new CursoBE();
+            curso.Nombre=nombre;
+            curso.Codigo=codigo;
+            curso.Creditos=creditos;
+            curso.Requisitos=requisitos;
+            curso.Ciclo=ciclo;
+            bool respuesta = cur.InsertarCurso(curso);
+            return respuesta;
+        }
+
+        static bool ActualizarCurso(int idCurso, String nombre, String codigo, int creditos, String requisitos, int ciclo)
+        {
+            CursoDALC cur = new CursoDALC();
+            CursoBE curso = new CursoBE();
+            curso.IdCurso = idCurso;
+            curso.Nombre = nombre;
+            curso.Codigo = codigo;
+            curso.Creditos = creditos;
+            curso.Requisitos = requisitos;
+            curso.Ciclo = ciclo;
+            bool respuesta = cur.ActualizarCurso(curso);
+            return respuesta;
+        }
+
+        static bool EliminarCurso(int id)
+        {
+            CursoDALC cur = new CursoDALC();
+            return cur.EliminarCurso(id);
+        }
+    }
+}
